50b549c6cc474ef5e9a807eb9d39b8d4a771e51f
gamejoon <gamejoon0325@naver.com>
2023-09-26 01:45:02 +0900

    update down

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -14,7 +14,8 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 coordinate = (((3, 47, 437, 493), (61, 104, 437, 493), (119, 162, 437, 493)),
               ((1, 50, 2, 55), (63, 110, 2, 55), (121, 167, 2, 55), (176, 223, 0, 53), (228, 285, 0, 53), (292, 341, 2, 55), (350, 396, 2, 55), (412, 459, 6, 55), (470, 517, 2, 53), (518, 574, 0, 53)),
               ((1, 57, 124, 178), (60, 107, 127, 178), (116, 163, 131, 180), (176, 223, 127, 180), (234, 284, 127, 180), (289, 346, 124, 178), (349, 396, 124, 178), (408, 453, 129, 180), (467, 514, 129, 180), (523, 572, 127, 180)),
-              ((5, 49, 66, 120), (63, 107, 66, 120), (121, 165, 66, 125), (179, 223, 66, 125), (237, 281, 66, 125), (294, 339, 66, 120), (352, 396, 66, 120), (410, 454, 66, 125), (468, 512, 64, 122), (526, 570, 66, 125)))
+              ((5, 49, 66, 120), (63, 107, 66, 120), (121, 165, 66, 125), (179, 223, 66, 125), (237, 281, 66, 125), (294, 339, 66, 120), (352, 396, 66, 120), (410, 454, 66, 125), (468, 512, 64, 122), (526, 570, 66, 125)),
+              ((5, 49, 187, 243), (63, 107, 192, 245), (121, 165, 187, 245), (179, 223, 187, 245), (236, 281, 187, 245), (294, 339, 187, 243), (352, 396, 192, 245), (410, 454, 187, 245), (467, 512, 187, 245), (525, 569, 187, 245)))
 
 frame = 0
 acceleration = [0, 0]
@@ -33,11 +34,11 @@ def character_motion(dir):
         character.clip_draw(coordinate[2][frame][0], coordinate[2][frame][2], (coordinate[2][frame][1] - coordinate[2][frame][0]), (coordinate[2][frame][3] - coordinate[2][frame][2]), x, y)
         frame = (frame + 1) % 10
     elif dir == "UP":
-        print("up")
         character.clip_draw(coordinate[3][frame][0], coordinate[3][frame][2], (coordinate[3][frame][1] - coordinate[3][frame][0]), (coordinate[3][frame][3] - coordinate[3][frame][2]), x, y)
         frame = (frame + 1) % 10
     elif dir == "DOWN":
-        print("down")
+        character.clip_draw(coordinate[4][frame][0], coordinate[4][frame][2], (coordinate[4][frame][1] - coordinate[4][frame][0]), (coordinate[4][frame][3] - coordinate[4][frame][2]), x, y)
+        frame = (frame + 1) % 10
 
 def handle_events():
 
@@ -62,6 +63,7 @@ def handle_events():
                 acceleration[1] = 10
             elif event.key == SDLK_DOWN:
                 character_dir = "DOWN"
+                acceleration[1] = -10
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT or event.key == SDLK_LEFT:
                 character_dir = "UNMOVE"
@@ -85,6 +87,8 @@ while running:
         x = 0 + 23
     elif y + 27 > TUK_HEIGHT - 1:
         y = TUK_HEIGHT - 1 - 27 
+    elif y - 27 < 0:
+        y = 0 + 27
 
     character_motion(character_dir)
     update_canvas()

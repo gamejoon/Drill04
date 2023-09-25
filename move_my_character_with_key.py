bbdadbc6ffd2b949f4051df41508be0821c5e91a
gamejoon <gamejoon0325@naver.com>
2023-09-26 05:00:39 +0900

    Drill04 제출 (2021180038 조영준)

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,40 +9,32 @@ running = True
 background = load_image("TUK_GROUND.png")
 
 character = load_image("animation_sheet.png")
-character_dir = "UNMOVE"
+character_dir = {"DOWN_UNMOVE" : 0, "DOWN" : 1, "UP_UNMOVE" : 2, "UP" : 3, "RIGHT_UNMOVE" : 4, "RIGHT" : 5, "LEFT_UNMOVE" : 6, "LEFT" : 7}
+dir = character_dir["DOWN_UNMOVE"]
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
 coordinate = (((3, 47, 437, 493), (61, 104, 437, 493), (119, 162, 437, 493)),
-              ((1, 50, 2, 55), (63, 110, 2, 55), (121, 167, 2, 55), (176, 223, 0, 53), (228, 285, 0, 53), (292, 341, 2, 55), (350, 396, 2, 55), (412, 459, 6, 55), (470, 517, 2, 53), (518, 574, 0, 53)),
-              ((1, 57, 124, 178), (60, 107, 127, 178), (116, 163, 131, 180), (176, 223, 127, 180), (234, 284, 127, 180), (289, 346, 124, 178), (349, 396, 124, 178), (408, 453, 129, 180), (467, 514, 129, 180), (523, 572, 127, 180)),
+              ((5, 49, 187, 243), (63, 107, 192, 245), (121, 165, 187, 245), (179, 223, 187, 245), (236, 281, 187, 245), (294, 339, 187, 243), (352, 396, 192, 245), (410, 454, 187, 245), (467, 512, 187, 245), (525, 569, 187, 245)),
+              ((5, 49, 312, 363), (5, 49, 312, 363), (5, 49, 312, 363)), # 똑같은 그림 3개
               ((5, 49, 66, 120), (63, 107, 66, 120), (121, 165, 66, 125), (179, 223, 66, 125), (237, 281, 66, 125), (294, 339, 66, 120), (352, 396, 66, 120), (410, 454, 66, 125), (468, 512, 64, 122), (526, 570, 66, 125)),
-              ((5, 49, 187, 243), (63, 107, 192, 245), (121, 165, 187, 245), (179, 223, 187, 245), (236, 281, 187, 245), (294, 339, 187, 243), (352, 396, 192, 245), (410, 454, 187, 245), (467, 512, 187, 245), (525, 569, 187, 245)))
+              ((5, 47, 249, 303), (61, 102, 249, 303), (120, 163, 249, 303)),
+              ((1, 50, 2, 55), (63, 110, 2, 55), (121, 167, 2, 55), (176, 223, 0, 53), (228, 285, 0, 53), (292, 341, 2, 55), (350, 396, 2, 55), (412, 459, 6, 55), (470, 517, 2, 53), (518, 574, 0, 53)),
+              ((8, 49, 374, 428), (65, 107, 374, 428), (124, 165, 374, 428)),
+              ((1, 57, 124, 178), (60, 107, 127, 178), (116, 163, 131, 180), (176, 223, 127, 180), (234, 284, 127, 180), (289, 346, 124, 178), (349, 396, 124, 178), (408, 453, 129, 180), (467, 514, 129, 180), (523, 572, 127, 180)))
 
 frame = 0
 acceleration = [0, 0]
 
-def character_motion(dir):
+def character_motion():
 
     global frame, x, y
 
-    if dir == "UNMOVE":
-        character.clip_draw(coordinate[0][frame][0], coordinate[0][frame][2], (coordinate[0][frame][1] - coordinate[0][frame][0]), (coordinate[0][frame][3] - coordinate[0][frame][2]), x, y)
-        frame = (frame + 1) % 3
-    elif dir == "RIGHT":
-        character.clip_draw(coordinate[1][frame][0], coordinate[1][frame][2], (coordinate[1][frame][1] - coordinate[1][frame][0]), (coordinate[1][frame][3] - coordinate[1][frame][2]), x, y)
-        frame = (frame + 1) % 10
-    elif dir == "LEFT":
-        character.clip_draw(coordinate[2][frame][0], coordinate[2][frame][2], (coordinate[2][frame][1] - coordinate[2][frame][0]), (coordinate[2][frame][3] - coordinate[2][frame][2]), x, y)
-        frame = (frame + 1) % 10
-    elif dir == "UP":
-        character.clip_draw(coordinate[3][frame][0], coordinate[3][frame][2], (coordinate[3][frame][1] - coordinate[3][frame][0]), (coordinate[3][frame][3] - coordinate[3][frame][2]), x, y)
-        frame = (frame + 1) % 10
-    elif dir == "DOWN":
-        character.clip_draw(coordinate[4][frame][0], coordinate[4][frame][2], (coordinate[4][frame][1] - coordinate[4][frame][0]), (coordinate[4][frame][3] - coordinate[4][frame][2]), x, y)
-        frame = (frame + 1) % 10
-
+    character.clip_draw(coordinate[dir][frame][0], coordinate[dir][frame][2], (coordinate[dir][frame][1] - coordinate[dir][frame][0]), (coordinate[dir][frame][3] - coordinate[dir][frame][2]), x, y)
+    frame = (frame + 1) % len(coordinate[dir])
+    
 def handle_events():
 
-    global running, character_dir, frame, acceleration
+    global running, dir, frame, acceleration
 
     events = get_events()
 
@@ -52,27 +44,35 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
-            elif event.key == SDLK_RIGHT:
-                character_dir = "RIGHT"
-                acceleration[0] = 10
-            elif event.key == SDLK_LEFT:
-                character_dir = "LEFT"
-                acceleration[0] = -10
             elif event.key == SDLK_UP:
-                character_dir = "UP"
+                dir = character_dir["UP"]
                 acceleration[1] = 10
             elif event.key == SDLK_DOWN:
-                character_dir = "DOWN"
+                dir = character_dir["DOWN"]
                 acceleration[1] = -10
+            elif event.key == SDLK_RIGHT:
+                dir = character_dir["RIGHT"]
+                acceleration[0] = 10
+            elif event.key == SDLK_LEFT:
+                dir = character_dir["LEFT"]
+                acceleration[0] = -10
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT or event.key == SDLK_LEFT:
-                character_dir = "UNMOVE"
-                frame = 0
-                acceleration[0] = 0
-            elif event.key == SDLK_UP or event.key == SDLK_DOWN:
-                character_dir = "UNMOVE"
+            if event.key == SDLK_UP:
+                dir = character_dir["UP_UNMOVE"]
                 frame = 0
                 acceleration[1] = 0
+            elif event.key == SDLK_DOWN:
+                dir = character_dir["DOWN_UNMOVE"]
+                frame = 0
+                acceleration[1] = 0
+            elif event.key == SDLK_RIGHT:
+                dir = character_dir["RIGHT_UNMOVE"]
+                frame = 0
+                acceleration[0] = 0
+            elif event.key == SDLK_LEFT:
+                dir = character_dir["LEFT_UNMOVE"]
+                frame = 0
+                acceleration[0] = 0
 
 while running:
     clear_canvas()
@@ -90,7 +90,7 @@ while running:
     elif y - 27 < 0:
         y = 0 + 27
 
-    character_motion(character_dir)
+    character_motion()
     update_canvas()
     delay(0.05)
 
